f7b136e204723ef9f8e01e293c91956a9c2df29d
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-04-17 01:19:56 +0100

    first commit

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "tfe" {}
 resource "tfe_workspace" "main" {
   name         = "test-student-1"
   organization = "prisma-cloud-compute"
-  # vcs_repo     = var.repo
+  vcs_repo     = var.repo
 }
 
